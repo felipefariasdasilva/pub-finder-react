68ac6ed0830d0fd1d69940f0710bc37ce6bc7c24
felipefariasdasilva <feliipefarias@outlook.com>
2020-03-04 12:34:41 -0300

    added search feature

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,7 +1,58 @@
 import GoogleMapReact from 'google-map-react';
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 class MapContainer extends Component{
+
+    handleSearch = (() => {
+
+        const { mapsApi, directionService, placesService } = this.state;
+        const filteredResults = [];
+        const timeLimitInMinutes = 180;
+        
+        // 1. create places request
+        const placesRequest = {
+            location: new mapsApi.LatLng(1.3521, 103.8198),
+            type: ['restaurant', 'cafe'],
+            query: 'ice cream',
+            rankBy: mapsApi.places.rankBy.DISTANCE,
+            //radius: 30000,
+        };
+
+        // 2. Search for ice cream shops. returns max 20 results
+        placesService.textSearch( placesRequest, ((response) => {
+
+            // 3. calculate traveling time for each location
+            for(let i = 0; i < response.length; i++){
+                const iceCreamPlace = response[i];
+                const { rating, name } = iceCreamPlace;
+                const address = iceCreamPlace.formatted_address; // e.g 80 mandai lake
+
+                // 4. create direction request for each location
+                const directionRequest = {
+                    origin: new mapsApi.LatLng(1.3521, 103.8198), // from
+                    destination: address, // to
+                    travelMode: 'DRIVING',
+                };
+
+                // 5. make request
+                directionService.route(directionRequest, ((result, status) => {
+                    if(status !== 'OK') { return }
+                    const travellingRoute = result.routes[0].legs[0];
+                    const travelingTimeInMinutes = travellingRoute.duration.value / 60;
+
+                    // 6. places within limit are added to results
+                    if(travelingTimeInMinutes < timeLimitInMinutes){
+                        filteredResults.push(name);
+                    }
+                }));
+
+                // 7. return results in state
+                this.setState({ searchResults: filteredResults });
+
+            }
+
+        }));
+    });
 
     render(){
         return(
@@ -21,10 +72,12 @@ class MapContainer extends Component{
                         }
                     }
                     yesIWantToUseGoogleMapApiInternals = {true}
-                    onGoogleApiLoaded = {({map, maps}) => this.apiHasLoaded(map, maps)}
+                    onGoogApiLoaded = {({map, maps}) => this.apiHasLoaded(map, maps)}
                 >
                 </GoogleMapReact>
             </section>
         );
     }
 }
+
+export default MapContainer;
